91d7bf5885a76656ff6c6ac724f4d88f0378b6d0
James F Moore <124608754+Kukukachuj@users.noreply.github.com>
2025-02-14 11:30:02 +0000

    M2T1 Start

---
+++ M2T1_Moore.cpp
@@ -0,0 +1,16 @@
+// csc 134
+// M2T1
+// Moore
+// 2/14/25
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "Hello World \n" << endl;
+    cout << endl;
+    return 0;
+}
