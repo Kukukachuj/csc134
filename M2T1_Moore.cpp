7cbc9c8b6cb2c9d4808d46cfe7ba67eb951bcfc6
James F Moore <124608754+Kukukachuj@users.noreply.github.com>
2025-02-14 11:56:08 +0000

    M2T1 DONE

--- M2T1_Moore.cpp
+++ M2T1_Moore.cpp
@@ -9,8 +9,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
-    cout << "Hello World \n" << endl;
-    cout << endl;
+    string name = "James's"; // Name 
+    int num_apples = 10;
+    double price_each = 0.25;
+
+
+    cout << "Welcome to  " << name << " apple farm!\n" << endl;
+    cout << "We have " << num_apples << " apples in stock" << endl;
+    cout << "Each apple costs $" << price_each << endl << endl;
+
+    double total_price = num_apples * price_each;
+    
+    cout << "The total price for all " << num_apples;
+    cout << " apples is: $" << total_price <<endl;
+    cout << "Thanks for coming by!" << endl;
     return 0;
 }
