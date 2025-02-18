fee2385d3c71b9a4a5368519ea3ae3dd401eb3e8
James F Moore <124608754+Kukukachuj@users.noreply.github.com>
2025-02-18 21:57:16 +0000

    before improvments

---
+++ M2HW1_Question4_Moore.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    string school, team;
+    cout << "Enter school name: " ;
+    getline (cin, school);
+    cout << "Enter team: ";
+    cin >> team;
+
+    cout << "Let\'s go " << school << endl;
+    cout << "Let\'s go " << school << endl;
+    cout << "Let\'s go " << school << endl;
+    cout << "Let\'s go " << team << endl;
+
+    return 0;
+
+}
