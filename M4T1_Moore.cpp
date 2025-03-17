2d6fc1d6bf033e20feb24010d27292023558196e
James F Moore <124608754+Kukukachuj@users.noreply.github.com>
2025-03-17 16:47:46 +0000

    Done

---
+++ M4T1_Moore.cpp
@@ -0,0 +1,23 @@
+/*
+CSC 134
+M4T1
+James Moore
+DATE:3/17/25
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int number =0;
+
+    while (number < 5)
+    {
+        cout << "Hello\n";
+        number++;
+    }
+    cout << "That's all!\n";
+
+    return 0;
+}
