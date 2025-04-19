8821a123014b7fd6c39a731b4180a071d2c81c5b
James F Moore <124608754+Kukukachuj@users.noreply.github.com>
2025-04-19 14:55:41 +0000

    done M5Hw1Question6 Started

---
+++ M5HW1_Question6_Moore.cpp
@@ -0,0 +1,43 @@
+/*
+CSC 134
+Assignment
+James Moore
+DATE:
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int question1(){
+    cout << "Fill Later";
+}
+int question2(){
+    cout << "Fill Later";
+}
+
+int question3(){
+    cout << "Fill Later";
+}
+
+int question4(){
+    cout << "Fill Later";
+}
+
+int question5(){
+    cout << "Fill Later";
+}
+
+int main(){
+
+    cout << "----------MENU----------" << endl;
+    cout << "1) " << endl;
+    cout << "2) " << endl;
+    cout << "1) " << endl;
+    cout << "1) " << endl;
+    cout << "1) " << endl;
+
+
+ 
+    return 0;
+}
